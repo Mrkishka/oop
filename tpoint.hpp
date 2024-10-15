6f8f4434f0256e39c155c2b6c446e50e483b5cd0
Miltov Danil <Dmiltov@bk.ru>
2024-10-15 09:15:56 +0700

    fff

--- tpoint.hpp
+++ tpoint.hpp
@@ -1,64 +1,56 @@
 #ifndef TPOINT_H
 #define TPOINT_H
 
-#include <cstdlib> // для функции rand()
-#include <ctime>   // для функции time()
+#include <cstdlib>
+#include <ctime>
 
 class tPoint
 {
 private:
-    int x, y;                      // Координаты точки
-    int screenWidth, screenHeight; // Размер экрана
+    int x, y;
+    int screenWidth, screenHeight;
     sf::Color color;
 
 public:
-    // Конструктор по умолчанию
     tPoint() : x(0), y(0), screenWidth(800), screenHeight(600) {}
 
-    // Конструктор с параметрами
     tPoint(int x, int y, int screenWidth, int screenHeight)
         : x(x), y(y), screenWidth(screenWidth), screenHeight(screenHeight)
     {
-        // Генерируем случайный цвет один раз при создании точки
         color = sf::Color(rand() % 256, rand() % 256, rand() % 256);
     }
-    // Геттер для цвета
     sf::Color getColor() const
     {
         return color;
     }
-    // Получить координаты
     int
     getX() const
     {
         return x;
     }
     int getY() const { return y; }
-    // Установить координаты
     void setX(int newX) { x = newX; }
     void setY(int newY) { y = newY; }
 
     void moveStraight(int &dx, int &dy, float speed)
     {
-        // Изменение координат с учетом скорости
         x += dx * speed;
         y += dy * speed;
 
-        // Проверка на отражение от стенок
         if (x <= 0 || x >= screenWidth)
         {
-            dx = -dx; // Меняем направление по оси X
+            dx = -dx;
             if (x < 0)
-                x = 0; // Фиксируем точку на границе
+                x = 0;
             if (x > screenWidth)
                 x = screenWidth;
         }
 
         if (y <= 0 || y >= screenHeight)
         {
-            dy = -dy; // Меняем направление по оси Y
+            dy = -dy;
             if (y < 0)
-                y = 0; // Фиксируем точку на границе
+                y = 0;
             if (y > screenHeight)
                 y = screenHeight;
         }
@@ -66,10 +58,10 @@ public:
 
     void moveRandom(float speed)
     {
-        static int dx = rand() % 21 - 10; // Случайное начальное движение по X
-        static int dy = rand() % 21 - 10; // Случайное начальное движение по Y
+        static int dx = rand() % 21 - 10;
+        static int dy = rand() % 21 - 10;
 
-        moveStraight(dx, dy, speed); // Используем уже исправленный метод
+        moveStraight(dx, dy, speed);
     }
 
 #endif // TPOINT_H
